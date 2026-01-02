bba0e0f2269f1c8979af495385b4ee1b62547a6d
Nazmul Hasan Shajib <shojib116@gmail.com>
2026-01-02 21:59:29 +0600

    add leaf node and tests for leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,6 +25,18 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
 
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
 
+        if self.tag == None:
+            return self.value
 
+        if self.props_to_html() != "": 
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+        return f"<{self.tag}>{self.value}</{self.tag}>"
